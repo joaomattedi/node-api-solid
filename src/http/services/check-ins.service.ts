baf5a314da385d4975b4bb7671bc46ec9197d860
João Mateus <69877286+joaomattedi@users.noreply.github.com>
2024-03-13 11:20:13 -0300

    feat: does not be able to check in twice on same day

--- src/http/services/check-ins.service.ts
+++ src/http/services/check-ins.service.ts
@@ -14,6 +14,18 @@ export class CheckInsService {
   constructor(private checkInsRepository: CheckInsRepository) {}
 
   async execute({ userId, gymId }: CheckInServiceRequest): Promise<CheckInServiceResponse> {
+    const checkInsFromUser = await this.checkInsRepository.findByUserId(userId);
+    const today = new Date()
+    const userHasAlreadyCheckedInToday = checkInsFromUser.some(checkIn => (
+      checkIn.created_at.getDate() === today.getDate()
+      && checkIn.created_at.getMonth() === today.getMonth()
+      && checkIn.created_at.getFullYear() === today.getFullYear()
+    ))
+
+    if (userHasAlreadyCheckedInToday) {
+      throw new Error('teste')
+    }
+
     const checkIn = await this.checkInsRepository.create({
       user_id: userId,
       gym_id: gymId
