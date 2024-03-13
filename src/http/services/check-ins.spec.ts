29d01a4fb819e6f07a9abb3303637d54e8721fbe
João Mateus <69877286+joaomattedi@users.noreply.github.com>
2024-03-13 11:17:45 -0300

    test: tdd for check ins

--- src/http/services/check-ins.spec.ts
+++ src/http/services/check-ins.spec.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckInsService } from './check-ins.service'
 
@@ -10,10 +10,48 @@ describe('Authenticate service', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckInsService(checkInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to check in', async () => {
-      const { checkIn } = await sut.execute({
+    const { checkIn } = await sut.execute({
+      userId: 'user-01',
+      gymId: 'gym-01'
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in twice on the same day', async () => {
+    vi.setSystemTime(new Date(2024,2,13,11,0,0))
+
+    await sut.execute({
+      userId: 'user-01',
+      gymId: 'gym-01'
+    })
+
+    await expect(() => sut.execute({
+      userId: 'user-01',
+      gymId: 'gym-01'
+    })).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should be able to check in twice but in different days', async () => {
+    vi.setSystemTime(new Date(2024,2,13,11,0,0))
+    
+    await sut.execute({
+      userId: 'user-01',
+      gymId: 'gym-01'
+    })
+
+    vi.setSystemTime(new Date(2024,2,14,11,0,0))
+
+    const { checkIn } = await sut.execute({
       userId: 'user-01',
       gymId: 'gym-01'
     })
