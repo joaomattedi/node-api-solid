d1f455422f0d81a672e90011dd2ea1c2b2818916
João Mateus <69877286+joaomattedi@users.noreply.github.com>
2024-03-17 18:11:32 -0300

    feat: find chek-in history service and tests

---
+++ src/http/services/fetch-user-check-ins-history.spec.ts
@@ -0,0 +1,36 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { FetchUserCheckInsHistoryService } from './fetch-user-check-ins-history.service'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: FetchUserCheckInsHistoryService // sut -> system under tests
+
+
+describe('Fecth Check-in History service', () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new FetchUserCheckInsHistoryService(checkInsRepository)
+  })
+
+  it('should be able to fetch check-in history', async () => {
+    await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_id: 'gym-01'
+    })
+
+    await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_id: 'gym-02'
+    })
+
+    const { checkIns } = await sut.execute({
+      userId: 'user-01'
+    })
+
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: 'gym-01' }),
+      expect.objectContaining({ gym_id: 'gym-02' })
+    ])
+  })
+})
