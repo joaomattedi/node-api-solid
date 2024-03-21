92de85f8ed0005fd564dcee4fbf554786b3b416d
João Mateus <69877286+joaomattedi@users.noreply.github.com>
2024-03-21 20:39:36 -0300

    feat: find check in by id

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -4,5 +4,6 @@ export interface CheckInsRepository {
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+  findById(checkInId: string): Promise<CheckIn | null>
   countByUserId(userId: string): Promise<number>
 }
--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -10,6 +10,16 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return this.items.filter(item => item.user_id === userId).length
   }
   
+  async findById(id: string) {
+    const checkIn = this.items.find(item => item.id === id)
+
+    if (!checkIn) {
+      return null
+    }
+
+    return checkIn
+  }
+
   async findManyByUserId(userId: string, page: number) {
     return this.items
       .filter(item => item.user_id === userId)
