f7ee4448f077e4afef40314ada3ebb71c81e406d
João Mateus <69877286+joaomattedi@users.noreply.github.com>
2024-03-29 16:51:34 -0300

    wip: profile route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from "fastify";
 import { registerUser } from "./controller/register.controller";
 import { authenticate } from "./controller/authenticate.controller";
+import { profile } from "./controller/profile.controller";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users',registerUser)
-
   app.post('/sessions',authenticate)
+  
+  /** Authenticated */
+  app.get('/me', profile)
 }
